d715836e8d5151269ed5cfe774e111e37cd451e5
Mehdi Bouaziz <mehdi@umeb>
2010-10-30 01:16:38 -0400

    Moved module Options from jsx to a separate file

--- jsx.ml
+++ jsx.ml
@@ -1,98 +1,5 @@
 open MyPervasives
 
-module Inputs =
-struct
-
-  type input_type =
-    | File of string
-    | InChannel of string * in_channel
-    | String of string * string
-
-  type code_type = JS | LJS | Env
-
-  let add_input inputs code_type input =
-    inputs := (code_type, input) :: !inputs
-
-  let add_file inputs code_type filename =
-    if filename = "STDIN" then
-      add_input inputs code_type (InChannel (filename, stdin))
-    else
-      let check_access x = Unix.access x [Unix.F_OK; Unix.R_OK] in
-      let _ = Unix.handle_unix_error check_access filename in
-      add_input inputs code_type (File filename)
-
-  let add_any_file inputs filename =
-    let code_type =
-      if filename = "STDIN" then
-	JS
-      else
-	match Filename.get_suffix filename with
-	| ".js" -> JS
-	| ".ljs" | ".jsl" | ".es5" -> LJS
-	| ".env" -> Env
-	| suffix -> failwith (sprintf "Unknown suffix \"%s\" for file \"%s\"" suffix filename)
-    in
-    add_file inputs code_type filename
-
-end
-
-module Options =
-struct
-
-  open Inputs
-
-  (* inputs *)
-  let inputs = ref []
-
-  (* options and their default values *)
-  let opt_eval = ref false
-  let opt_features = ref false
-  let opt_pretty = ref false
-  let opt_xeval = ref true
-
-  (* 'a -> 'a functions for options *)
-  let ifoption opt_ref f x = if !opt_ref then f x else x
-
-  (* need OCaml 3.12 to get rid of f x ? *)
-  let if_eval f x = ifoption opt_eval f x
-  let if_features f x = ifoption opt_features f x
-  let if_pretty f x = ifoption opt_pretty f x
-  let if_xeval f x = ifoption opt_xeval f x
-
-
-  let arg_speclist =
-    let speclist =
-      [
-	"-js", Arg.String (add_file inputs JS), "<file> Load <file> as JavaScript";
-	"-ljs", Arg.String (add_file inputs LJS), "<file> Load <file> as LambaJS.ES5";
-	"-env", Arg.String (add_file inputs Env), "<file> Load <file> as environment (LambdaJS.ES5)";
-      ]
-    in
-    let boolspeclist =
-      [
-	"eval", opt_eval, "evaluation of code";
-	"features", opt_features, "listing of used features";
-	"pretty", opt_pretty, "pretty printing of code";
-	"xeval", opt_xeval, "symbolic evaluation of code";
-      ]
-    in
-    let turn l (name, r, meaning) =
-      ("-" ^ name, Arg.Set r, " Turn on " ^ meaning ^ (if !r then " (default)" else ""))
-      ::("-no-" ^ name, Arg.Clear r, " Turn off " ^ meaning ^ (if !r then "" else " (default)"))
-      ::l
-    in
-    Arg.align (List.sort Pervasives.compare (List.fold_left turn speclist boolspeclist))
-
-  let arg_usage = sprintf "Usage: %s [options] <file> ..." (Filename.basename Sys.executable_name)
-
-  let arg_parse () = Arg.parse arg_speclist (add_any_file inputs) arg_usage
-
-  let error_usage errmsg =
-    Arg.usage arg_speclist (errmsg ^ "\n\n" ^ arg_usage);
-    exit 1
-
-end
-
 
 module Parsers =
 struct
@@ -110,19 +17,20 @@ struct
     in f x
 
   let from_input (code_type, input_type) prev_ljs =
+    let open Options.Inputs in
     let fname, lexbuf = match input_type with
-    | Inputs.File fname -> fname, Lexing.from_channel (open_in fname)
-    | Inputs.InChannel (fname, cin) -> fname, Lexing.from_channel cin
-    | Inputs.String (fname, s) -> fname, Lexing.from_string s
+    | File fname -> fname, Lexing.from_channel (open_in fname)
+    | InChannel (fname, cin) -> fname, Lexing.from_channel cin
+    | String (fname, s) -> fname, Lexing.from_string s
     in
     lexbuf.Lexing.lex_curr_p <- { lexbuf.Lexing.lex_curr_p with Lexing.pos_fname = fname };
     let eseq ljs1 ljs2 =
       LambdaJS.Syntax.ESeq (LambdaJS.Prelude.dummy_pos, ljs1, ljs2)
     in
     match code_type with
-    | Inputs.JS -> try_parse JS.Parser.program JS.Lexer.token lexbuf (JS.Interm.from_javascript @> LambdaJS.Desugar.ds_top @> eseq prev_ljs)
-    | Inputs.LJS -> try_parse LambdaJS.Parser.prog LambdaJS.Lexer.token lexbuf (LambdaJS.Syntax.raw_of_fine @> eseq prev_ljs)
-    | Inputs.Env -> try_parse LambdaJS.Parser.env LambdaJS.Lexer.token lexbuf ((|>) prev_ljs)
+    | JS -> try_parse JS.Parser.program JS.Lexer.token lexbuf (JS.Interm.from_javascript @> LambdaJS.Desugar.ds_top @> eseq prev_ljs)
+    | LJS -> try_parse LambdaJS.Parser.prog LambdaJS.Lexer.token lexbuf (LambdaJS.Syntax.raw_of_fine @> eseq prev_ljs)
+    | Env -> try_parse LambdaJS.Parser.env LambdaJS.Lexer.token lexbuf ((|>) prev_ljs)
 
   let from_inputs input_list =
     LambdaJS.Syntax.EConst (LambdaJS.Prelude.dummy_pos, JS.Syntax.CUndefined)
@@ -135,7 +43,7 @@ let main () =
   Options.arg_parse ();
   if !Options.inputs = [] then
     Options.error_usage "No input";
-  if List.for_all (function (Inputs.Env, _) -> false | _ -> true) !Options.inputs then
+  if List.for_all (function (Options.Inputs.Env, _) -> false | _ -> true) !Options.inputs then
     warning "Desugaring without environment";
   let raw_ljs = Parsers.from_inputs (!Options.inputs) in
   let fine_ljs = LambdaJS.Desugar.desugar raw_ljs in
---
+++ options.ml
@@ -0,0 +1,81 @@
+open MyPervasives
+
+module Inputs =
+struct
+
+  type input_type =
+    | File of string
+    | InChannel of string * in_channel
+    | String of string * string
+
+  type code_type = JS | LJS | Env
+
+  let add_input inputs code_type input =
+    inputs := (code_type, input) :: !inputs
+
+  let add_file inputs code_type filename =
+    if filename = "STDIN" then
+      add_input inputs code_type (InChannel (filename, stdin))
+    else
+      let check_access x = Unix.access x [Unix.F_OK; Unix.R_OK] in
+      let _ = Unix.handle_unix_error check_access filename in
+      add_input inputs code_type (File filename)
+
+  let add_any_file inputs filename =
+    let code_type =
+      if filename = "STDIN" then
+	JS
+      else
+	match Filename.get_suffix filename with
+	| ".js" -> JS
+	| ".ljs" | ".jsl" | ".es5" -> LJS
+	| ".env" -> Env
+	| suffix -> failwith (sprintf "Unknown suffix \"%s\" for file \"%s\"" suffix filename)
+    in
+    add_file inputs code_type filename
+
+end
+
+
+open Inputs
+
+(* inputs *)
+let inputs = ref []
+
+(* options and their default values *)
+let opt_eval = ref false
+let opt_features = ref false
+let opt_pretty = ref false
+let opt_xeval = ref true
+
+
+let arg_speclist =
+  let speclist =
+    [
+      "-js", Arg.String (add_file inputs JS), "<file> Load <file> as JavaScript";
+      "-ljs", Arg.String (add_file inputs LJS), "<file> Load <file> as LambaJS.ES5";
+      "-env", Arg.String (add_file inputs Env), "<file> Load <file> as environment (LambdaJS.ES5)";
+    ]
+  in
+  let boolspeclist =
+    [
+      "eval", opt_eval, "evaluation of code";
+      "features", opt_features, "listing of used features";
+      "pretty", opt_pretty, "pretty printing of code";
+      "xeval", opt_xeval, "symbolic evaluation of code";
+    ]
+  in
+  let turn l (name, r, meaning) =
+    ("-" ^ name, Arg.Set r, " Turn on " ^ meaning ^ (if !r then " (default)" else ""))
+    ::("-no-" ^ name, Arg.Clear r, " Turn off " ^ meaning ^ (if !r then "" else " (default)"))
+    ::l
+  in
+  Arg.align (List.sort Pervasives.compare (List.fold_left turn speclist boolspeclist))
+
+let arg_usage = sprintf "Usage: %s [options] <file> ..." (Filename.basename Sys.executable_name)
+
+let arg_parse () = Arg.parse arg_speclist (add_any_file inputs) arg_usage
+
+let error_usage errmsg =
+  Arg.usage arg_speclist (errmsg ^ "\n\n" ^ arg_usage);
+  exit 1
